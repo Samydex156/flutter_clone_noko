2695725747825f83ba2c785d18adce19e065ccd1
samydex156 <samydex156@gmail.com>
2025-12-29 18:45:57 -0400

    Clon de Noko 01!

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_impresora
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_impresora/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_impresora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
